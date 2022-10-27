df9e149e47b6bd3af2083c3bb2b1dba13b387c2c
kennedy1996 <87372917+kennedy1996@users.noreply.github.com>
2022-10-27 18:45:44 +0100

    Initial commit

---
+++ app/src/main/java/com/qrcode/QrcodeActivity.kt
@@ -0,0 +1,11 @@
+package com.qrcode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QrcodeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.qrcode_activity)
+    }
+}
